10e1cb349abc7764cbb978ed3d5a082ce848c7b4
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2025-02-18 01:21:18 +0530

    first commit created user model

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("SignUp");
+};
+export const login = (req, res) => {
+  res.send("login");
+};
+export const logout = (req, res) => {
+  res.send("logout");
+};
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_@]).{8,}$/.test(
+            value
+          );
+        },
+        message:
+          "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit and one special character (_ or @).",
+      },
+    },
+    profilePic: { type: String, default: "" },
+    delete_type: { type: Number, default: 0 },
+    deletedAt: { type: Date, default: null },
+  },
+  { Timestamps: true }
+);
+
+userSchema.methods.softDelete = async function () {
+  this.delete_type = 1;
+  this.deletedAt = new Date();
+  await this.save();
+};
+
+userSchema.methods.restoreUser = async function () {
+  this.delete_type = 0;
+  this.deletedAt = null;
+  await this.save();
+};
+
+userSchema.static.methods.findActiveUser = async function (userId) {
+  return await this.findOne({ _id: userId, delete_type: 0 });
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
