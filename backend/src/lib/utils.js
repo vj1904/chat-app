6692281117179736a3a59ee93d2fd482d0cfea05
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2025-04-01 02:27:27 +0530

    fixed jwt issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -13,7 +13,7 @@ export const genrateJwtToken = (userId, res) => {
     maxAge: 3 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "None",
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
   });
 
   return token;
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
