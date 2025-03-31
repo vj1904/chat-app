a5e3d9e932a905c8cdf014a0b0e2b53d9b297deb
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2025-03-31 18:12:49 +0530

    fixed PORT issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -23,11 +23,11 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Welcome User");
+});
+
 server.listen(PORT, () => {
   console.log("server is running on port:", PORT);
   connectDB();
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome User");
 });
