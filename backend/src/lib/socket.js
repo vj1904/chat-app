8db567612ef490fce7289770e41b3e3592ed0b4c
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2025-03-31 21:43:40 +0530

    fixed cors issue

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,30 +7,36 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chat-app-five-ecru.vercel.app/"],
+    origin: ["http://localhost:5173", "https://chat-app-five-ecru.vercel.app"],
   },
 });
 
+// used to store online users
+const userSocketMap = {};
+
+// Get receiver's socket ID
 export function getRecieverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {};
 
 // listen for connections
 io.on("connection", (socket) => {
   console.log("A user has connected: ", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    socket.userId = userId; 
+  }
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    if (socket.userId) {
+      delete userSocketMap[socket.userId];
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    }
   });
 });
 
